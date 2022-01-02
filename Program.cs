464b8d9d7fe0a53990b7dae240c76ac2087b6aa2
Axro-psy <76491158+Axro-psy@users.noreply.github.com>
2022-01-02 16:24:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console; //чтобы не писать каждый раз Console
+namespace helloWORLD_domashka_1
+{
+    class Program
+    {
+        // Ctrl+space волшебные горячие клавиши, чтобы открылась помогашка
+
+        /// <summary>
+        /// XML комментарий, пока не знаю зачем, но запишем
+        /// </summary>
+        /// <param name="args">параметры запуска</param>
+        /// 
+        static void Main(string[] args) //точка входа
+        {
+            WriteLine("Hello, world!!!"); //вывод текста в консоль
+
+
+            ReadKey(); 
+                //выяснить как вставить определенную кнопку в код
+
+            //А и Б сидели на трубе, 
+            //    А упало Б пропало
+            //    что осталось на трубе?
+        }
+    }
+}
